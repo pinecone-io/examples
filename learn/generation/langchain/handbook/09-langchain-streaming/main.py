11f60624ef0b0571d9eb86271030de9be2abed20
James Briggs <35938317+jamescalam@users.noreply.github.com>
2023-09-28 11:48:59 +0200

    update to use async

--- learn/generation/langchain/handbook/09-langchain-streaming/main.py
+++ learn/generation/langchain/handbook/09-langchain-streaming/main.py
@@ -1,6 +1,6 @@
 import os
-from threading import Thread
-from typing import Generator
+import asyncio
+from typing import Any
 
 import uvicorn
 from fastapi import FastAPI, Body
@@ -11,39 +11,19 @@ from pydantic import BaseModel
 from langchain.agents import AgentType, initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferWindowMemory
-from langchain.callbacks.base import BaseCallbackHandler
+from langchain.callbacks.streaming_aiter import AsyncIteratorCallbackHandler
+from langchain.callbacks.streaming_stdout_final_only import FinalStreamingStdOutCallbackHandler
+from langchain.schema import LLMResult
 
 app = FastAPI()
 
-class QueueCallbackHandler(BaseCallbackHandler):
-    def __init__(self, queue: Queue):
-        self.queue = queue
-        self.content: str = ""
-        self.final_answer: bool = False
-
-    def on_llm_new_token(self, token: str, **kwargs: any) -> None:
-        self.content += token
-        # if we passed the final answer, we put tokens in queue
-        if self.final_answer:
-            if '"action_input": "' in self.content:
-                if token not in ['"', "}"]:
-                    self.queue.put(token)
-        elif "Final Answer" in self.content:
-            self.final_answer = True
-            self.content = ""
-
-# queue is our communication between threads
-queue = Queue()
-# we use this for signaling the end of the queue
-job_done = "<END-STREAM>"
-
-# initialize the agent
+# initialize the agent (we need to do this for the callbacks)
 llm = ChatOpenAI(
     openai_api_key=os.getenv("OPENAI_API_KEY"),
     temperature=0.0,
     model_name="gpt-3.5-turbo",
     streaming=True,  # ! important
-    callbacks=[QueueCallbackHandler(queue)]  # ! important
+    callbacks=[]  # ! important (but we will add them later)
 )
 memory = ConversationBufferWindowMemory(
     memory_key="chat_history",
@@ -62,30 +42,52 @@ agent = initialize_agent(
     return_intermediate_steps=False
 )
 
-# this handles the call to the agent, followed by job_done token
-def call(query: str, queue: Queue) -> None:
-    agent.run(query)
-    queue.put(job_done)
+class AsyncCallbackHandler(AsyncIteratorCallbackHandler):
+    content: str = ""
+    final_answer: bool = False
+
+    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        self.content += token
+        # if we passed the final answer, we put tokens in queue
+        if self.final_answer:
+            if '"action_input": "' in self.content:
+                if token not in ['"', "}"]:
+                    self.queue.put_nowait(token)
+        elif "Final Answer" in self.content:
+            self.final_answer = True
+            self.content = ""
+    
+    async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        if self.final_answer:
+            self.content = ""
+            self.final_answer = False
+            self.done.set()
+        else:
+            self.content = ""
+
+async def run_call(query: str, stream_it: AsyncCallbackHandler):
+    # assign callback handler
+    agent.agent.llm_chain.llm.callbacks = [stream_it]
+    # now query
+    await agent.acall(inputs={"input": query})
 
 # request input format
 class Query(BaseModel):
     text: str
 
-def stream(query: Query) -> Generator:
-    """This function is what allows us to stream"""
-    # move the agent task to another thread
-    thread = Thread(target=call, args=(query.text, queue))
-    thread.start()
-    # on main thread, yield the results from queue to StreamingResponse
-    while True:
-        result = queue.get()
-        if result is job_done:
-            break
-        yield result
+async def create_gen(query: str, stream_it: AsyncCallbackHandler):
+    task = asyncio.create_task(run_call(query, stream_it))
+    async for token in stream_it.aiter():
+        yield token
+    await task
 
 @app.post("/chat")
-async def chat(query: Query = Body(...)):
-    return StreamingResponse(stream(query), media_type="text/event-stream")
+async def chat(
+    query: Query = Body(...),
+):
+    stream_it = AsyncCallbackHandler()
+    gen = create_gen(query.text, stream_it)
+    return StreamingResponse(gen, media_type="text/event-stream")
 
 @app.get("/health")
 async def health():
