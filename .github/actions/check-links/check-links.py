9b8990b30589ba80a8a8526d99024c0e01649172
Jenna Pederson <jenna@pinecone.io>
2026-09-01 11:18:25 -0500

    Fixed broken link; Added another domain to ignore list.

--- .github/actions/check-links/check-links.py
+++ .github/actions/check-links/check-links.py
@@ -28,6 +28,7 @@ ignore_links = [
     "app.pinecone.io",  # we don't need to spam our own homepage
     "q4cdn.com",  # CDN returns 451 for HEAD requests but works with GET
     "prod-1-data.ke.pinecone.io",  # internal API endpoint requiring auth
+    "aistudio.google.com",  # redirects to Google sign-in, requires auth
 ]
 
 known_good_links = set(known_good)
